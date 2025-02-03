8eab0ac89360043ae2ab7d8b486bae716c38b429
Mark-168 <isseitakei11@gmail.com>
2025-02-03 14:50:38 +0900

    update test.bash

--- test.bash
+++ test.bash
@@ -9,14 +9,14 @@ ng () {
 
 res=0
 
-out=$(seq 5 | ./plus)
+out=$(seq 5 | ./PSD)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-out=$(echo あ | ./plus)
+out=$(echo あ | ./PSD)
 [ "$?" = 1 ]       ng "$LINENO"
 [ "${out}" = "" ]  ng "$LINENO"
 
-out=$(echo  | ./plus)
+out=$(echo  | ./PSD)
 [ "$?" = 1 ]       ng "$LINENO"
 [ "${out}" = "" ]  ng "$LINENO"
 
